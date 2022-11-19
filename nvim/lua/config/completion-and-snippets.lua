3679b56979ebd598011770714ce239ea253e386a
Anand Thakker <anandthakker@users.noreply.github.com>
2022-11-19 09:52:00 -0500

    Disable completion within comments

--- nvim/lua/config/completion-and-snippets.lua
+++ nvim/lua/config/completion-and-snippets.lua
@@ -1,19 +1,12 @@
--- vim.g.coq_settings = {
---   ['auto_start'] = 'shut-up',
---   ['display.ghost_text.enabled'] = false
--- }
-
--- nvim-cmp supports additional completion capabilities
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- luasnip setup
-local luasnip = require "luasnip"
-
 -- nvim-cmp setup
 local cmp = require "cmp"
 vim.o.completeopt = "menu,menuone,noselect"
+local context = require "cmp.config.context"
 cmp.setup {
+  enabled = function()
+    -- disable completion if the cursor is `Comment` syntax group.k
+    return (not context.in_syntax_group('Comment') and not context.in_treesitter_capture("comment"))
+  end,
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
@@ -51,68 +44,3 @@ cmp.setup {
   }
 }
 
---[[
-local cmp = require "cmp"
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    },
-    ["<Tab>"] = cmp.mapping(
-      function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          feedkey("<C-n>", "n")
-        elseif vim.fn["vsnip#available"]() == 1 then
-          feedkey("<Plug>(vsnip-expand-or-jump)", "")
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-        end
-      end,
-      {"i", "s"}
-    ),
-    ["<S-Tab>"] = cmp.mapping(
-      function()
-        if vim.fn.pumvisible() == 1 then
-          feedkey("<C-p>", "n")
-        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-          feedkey("<Plug>(vsnip-jump-prev)", "")
-        end
-      end,
-      {"i", "s"}
-    )
-  },
-  sources = {
-    {name = "nvim_lsp"},
-    {name = "buffer"},
-    {name = "path"},
-    {name = "vsnip"}
-  }
-}
-]] --
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -35,7 +35,7 @@ for _, server in ipairs(servers) do
     flags = {
       debounce_text_changes = 150
     },
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
   }
 end
 
